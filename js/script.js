d83fed3bb6f68cfe1a050e3f0e7641983c066944
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-05-17 23:20:20 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log('Olá, mundo!');
+
+var title = document.querySelector('h1');
+title.textContent = 'Modificado por Fabiany Sousa';
+
+var x = 10;
+console.log(x);
+console.log('O valor de x + 10 é ' + (x + 10));
