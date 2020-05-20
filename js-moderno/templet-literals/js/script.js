f977ba604fbec4e5124f50ddbf3afd3f739f4c60
Fabiany de Sousa <fabianydesousa@gmail.com>
2020-05-20 11:38:10 -0300

    ES6+ templet-literals
    exemplos com templet-literals

---
+++ js-moderno/templet-literals/js/script.js
@@ -0,0 +1,10 @@
+// template literals
+
+const name = 'Fabiany';
+const surName = 'Sousa';
+const text1 = 'Meu nome é ' + name + ' ' + surName;
+
+const text2 = 'Meu nome é  $(name)  $(surName)';
+
+console.log(text1);
+console.log(text2);
